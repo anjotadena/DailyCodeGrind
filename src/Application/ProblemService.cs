6a31e96d55f526c73080ed0c2fcc2ce1122e5ea2
anjotadena <tadena.anjo@gmail.com>
2026-02-02 10:35:04 +0800

    Update Presentation layer

---
+++ src/Application/ProblemService.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities;
+using Infrastructure;
+
+namespace Application;
+
+public class ProblemService
+{
+	private readonly FileProblemRepository _repository;
+
+	public ProblemService(FileProblemRepository repository)
+	{
+		_repository = repository;
+	}
+
+	public IEnumerable<ProblemMetadata> GetAllProblems()
+	{
+		return _repository.GetAllProblems();
+	}
+}
